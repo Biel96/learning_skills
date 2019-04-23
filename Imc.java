e2f90a51749042f63f8ca76b6a48e445f351630c
Gabriel Gomes <49540452+Biel96@users.noreply.github.com>
2019-04-23 08:55:31 -0300

    Add files via upload

---
+++ Imc.java
@@ -0,0 +1,37 @@
+package learning_skills;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Imc {
+
+	public static void main(String[] args) {
+// Cálculo de IMC
+		double pessoaAltura = 0, pessoaPeso = 0, imc;
+		Scanner read = new Scanner(System.in);
+
+		System.out.println("Digite seu peso: ");
+		pessoaPeso = read.nextDouble();
+		System.out.println("Digite sua altura: ");
+		pessoaAltura = read.nextDouble();
+
+		imc = (pessoaPeso) / Math.pow(pessoaAltura, 2);
+
+		String resultado = String.format("%.2f", imc);
+		System.out.println("Seu IMC é: " + resultado);
+
+		if (imc >= 18.5 && imc < 25) {
+			System.out.println("IMC normal");
+		} else if (imc > 25 && imc < 30) {
+			System.out.println("Você está acima do peso");
+		} else if (imc > 30 && imc < 35) {
+			System.out.println("Você está obeso");
+		} else {
+			System.out.println("Você quebrou a balança!");
+
+		}
+
+		read.close();
+	}
+
+}
